08e7d016ca459da189dda52209f95113f26e98a7
Sam Pirate <semen.chugunov@metalab.ifmo.ru>
2022-11-30 15:56:19 +0300

    Написана функция capacitance

--- functions_for_comsol.py
+++ functions_for_comsol.py
@@ -25,21 +25,16 @@ def maxwell_to_mutual(matrix, decimals):
     return new_matrix
 
 
-
-
-
 # генерирует детали
 def creating_details(matrix, size: int, a, model, geometry, physic, terminal_other, initial_values, terminal_leg):
     # a - сторона квадрата
     k = a/size    # цена деления
     list_terminal_leg = []
     list_terminal_other = []
-
     node = model/'geometry'/geometry
     node_other = model/'physics'/physic/terminal_other
     node_values = model/'physics'/physic/initial_values
     node_leg = model/'physics'/physic/terminal_leg
-
     for i in range(0, size):
         for j in range(0, size):
             if matrix[i, j] != 0:
@@ -75,17 +70,17 @@ def creating_details(matrix, size: int, a, model, geometry, physic, terminal_oth
 
 
 # генерирует землю
-def creating_ground(model, radius, geometry):
-    node_local = model/'geometry'/geometry
-    node_local.create('Circle', name='circle_ground')
-    node_local = model / 'geometry' / geometry/'circle_ground'
-    node_local.property('r', radius)
-    node_local.property('x', str(-radius))
-    node_local.property('y', str(-radius))
-    node_local.property('type', 'solid')
-    node_local.property('selresultshow', 'all')
-    node_local.property('color', '2')
-    return
+# def creating_ground(model, radius, geometry):
+#     node_local = model/'geometry'/geometry
+#     node_local.create('Circle', name='circle_ground')
+#     node_local = model / 'geometry' / geometry/'circle_ground'
+#     node_local.property('r', radius)
+#     node_local.property('x', str(-radius))
+#     node_local.property('y', str(-radius))
+#     node_local.property('type', 'solid')
+#     node_local.property('selresultshow', 'all')
+#     node_local.property('color', '2')
+#     return
 
 
 # смена радиуса земли
@@ -95,49 +90,47 @@ def change_radius_out(model, geometry, circle_ground, r):
     return
 
 
-# считает матрицу
+# считает емкости
 def evaluate_matrix(model, dataset):
-
-    # print(model.evaluate('es.C11', 'pF', 'Study 2//Parametric Solutions 2'))
-    # print(model.evaluate('es.C22', 'pF'))
-    # print(model.evaluate('es.C12', 'pF'))
-
-    answer = np.zeros((2, 2))
-    for i in range(1, 3):
-        for j in range(1, 3):
-            answer[i-1][j-1] = model.evaluate('es.C'+str(i)+str(j), 'pF', dataset=dataset)
+    c11 = model.evaluate('es.C11', 'pF', dataset=dataset)
+    c21 = model.evaluate('es.C21', 'pF', dataset=dataset)
+    c22 = model.evaluate('es.C22', 'pF', dataset='Study 1//Solution 1')
+    c11_new = c11 + c21
+    c21_new = (-1)*c21
+    c22_new = c22 + c21
+    answer = np.array([c11_new, c21_new, c22_new])
     return answer
 
 
 
 
-if __name__ == "__main__":
-    # размер квадратной матрицы
-    N = 7
-    # matrix - матрица, count - кол-во деталей
-    matrix0, count = gt.parts(gt.smooth(np.random.randint(0, 2, (N, N))))
-    radius1 = 0.1
-    geometry1 = 'geometry_test'
-    circle_ground1 = 'circle_ground'
-    ground1 = 'ground'
-    terminal_leg1 = 'terminal_leg'
-    terminal_other1 = 'terminal_other'
-    initial_values1 = 'Initial Values 1'
-    physics1 = 'Electrostatics'
-    client = mph.start()
-    model1 = client.load("test.mph")
-    remove_details(model1, geometry1)
-    model1.build(geometry1)
-    model1.save()
-
-
-    creating_details(matrix0, N, radius1*2, model1, geometry1, physics1, terminal_other1, initial_values1, terminal_leg1)
-    change_radius_out(model1, geometry1, circle_ground1, 0.2)
-    model1.build(geometry1)
-    model1.save()
-    model1.solve('Study 1')
-    m = evaluate_matrix(model1, 'Study 1//Parametric Solutions 2')
-    print(m)
-
-    model1.save()
+# if __name__ == "__main__":
+#     # размер квадратной матрицы
+#     N = 7
+#     # matrix - матрица, count - кол-во деталей
+#     matrix0, count = gt.parts(gt.smooth(np.random.randint(0, 2, (N, N))))
+#     radius1 = 0.1
+#     geometry1 = 'geometry_test'
+#     circle_ground1 = 'circle_ground'
+#     ground1 = 'ground'
+#     terminal_leg1 = 'terminal_leg'
+#     terminal_other1 = 'terminal_other'
+#     initial_values1 = 'Initial Values 1'
+#     physics1 = 'Electrostatics'
+#     client = mph.start()
+#     model1 = client.load("test.mph")
+#     remove_details(model1, geometry1)
+#     model1.build(geometry1)
+#     model1.save()
+#
+#
+#     creating_details(matrix0, N, radius1*2, model1, geometry1, physics1, terminal_other1, initial_values1, terminal_leg1)
+#     change_radius_out(model1, geometry1, circle_ground1, 0.2)
+#     model1.build(geometry1)
+#     model1.save()
+#     model1.solve('Study 1')
+#     m = evaluate_matrix(model1, 'Study 1//Parametric Solutions 2')
+#     print(m)
+#
+#     model1.save()
 
--- main_comsol.py
+++ main_comsol.py
@@ -4,12 +4,27 @@ import geomety_test as gt
 import functions_for_comsol as ffc
 client = mph.start()
 model0 = client.load("test.mph")
+geometry0 = 'geometry_test'
+circle_ground0 = 'circle_ground'
+ground0 = 'ground'
+terminal_leg0 = 'terminal_leg'
+terminal_other0 = 'terminal_other'
+initial_values0 = 'Initial Values 1'
+physics0 = 'Electrostatics'
 
-def capacitance():
-    # вход: геометрия в виде матрицы, радиус земли
-    # общая функция 24-36, убрать save
-    # вектор с емкостями
-    return
+
+def capacitance(matrix, radius):
+    n = np.shape(matrix)[0]
+    ffc.remove_details(model0, geometry0)
+    ffc.creating_details(matrix, n, radius * 2, model0, geometry0, physics0, terminal_other0, initial_values0,
+                         terminal_leg0)
+    ffc.change_radius_out(model0, geometry0, circle_ground0, radius)
+    model0.build(geometry0)
+    model0.solve('Study 1')
+    vector = ffc.evaluate_matrix(model0, 'Study 1//Parametric Solutions 2')
+    return vector
+
+
 if __name__ == "__main__":
     geometry0 = 'geometry_test'
     circle_ground0 = 'circle_ground'
@@ -18,22 +33,22 @@ if __name__ == "__main__":
     terminal_other0 = 'terminal_other'
     initial_values0 = 'Initial Values 1'
     physics0 = 'Electrostatics'
-
-    N = 7
-    matrix0, count = gt.parts(gt.smooth(np.random.randint(0, 2, (N, N))))
-    radius1 = 0.1
-
-    ffc.remove_details(model0, geometry0)
-
-    ffc.creating_details(matrix0, N, radius1*2, model0, geometry0, physics0, terminal_other0, initial_values0, terminal_leg0)
-
-    ffc.change_radius_out(model0, geometry0, circle_ground0, radius1*3)
-
-    model0.build(geometry0)
-    model0.save()
-    model0.solve('Study 1')
-    m = ffc.evaluate_matrix(model0, 'Study 1//Parametric Solutions 2')
-    model0.save()
-    print('Maxwell capacitance(matrix):')
-    print(m)
+    # N = 7
+    # matrix0, count = gt.parts(gt.smooth(np.random.randint(0, 2, (N, N))))
+    # radius1 = 0.1
+    #
+    # ffc.remove_details(model0, geometry0)
+    #
+    # ffc.creating_details(matrix0, N, radius1*2, model0, geometry0, physics0, terminal_other0, initial_values0, terminal_leg0)
+    #
+    # ffc.change_radius_out(model0, geometry0, circle_ground0, radius1*3)
+    #
+    # model0.build(geometry0)
+    # model0.save()
+    # model0.solve('Study 1')
+    # m = ffc.evaluate_matrix(model0, 'Study 1//Parametric Solutions 2')
+    # print(m)
+    # model0.save()
+    # print('Maxwell capacitance(matrix):')
+    # print(m)
 
